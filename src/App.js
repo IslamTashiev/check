6b4d8c0af2f02ccad3bc86ee2ae87395706111d7
IslamTashiev <ueaten04@gmail.com>
2024-08-18 16:43:11 +0600

    change config

--- src/App.js
+++ src/App.js
@@ -75,6 +75,12 @@ function App() {
             <Card data={item} setSelectedCard={setSelectedCard} products={products} />
           </div>
         ))}
+        <div class="card-item add">
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 6.6665V25.3332" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M6.66602 16H25.3327" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        </div>
       </div>
 
       {selectedCard && (
--- src/components/Check.js
+++ src/components/Check.js
@@ -1,18 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Table from "./Table";
 import qrCode from "../assets/plov_alamedin.svg";
 
 const Check = ({ data, setShowCheck }) => {
+  const [totalSum, setTotalSum] = useState(0);
+  const [newData, setNewData] = useState(data);
+
   useEffect(() => {
+    // setTotalSum(data.map((el) => el.sum).reduce((acc, el) => acc + el, 0));
+    const res = [
+      ...data,
+      {
+        title: "Услуга",
+        price: "15%",
+        id: 1345,
+        sum: Math.floor(data.map((el) => el.sum).reduce((acc, el) => acc + el, 0) * 0.15),
+      },
+    ];
+    setNewData(res);
+    setTotalSum(res.map((el) => el.sum).reduce((acc, el) => acc + el, 0));
     setTimeout(() => {
       window.print();
     }, 100);
-    setTimeout(() => {
-      setShowCheck(false);
-    }, 1000);
+    // setTimeout(() => {
+    //   setShowCheck(false);
+    // }, 1000);
   }, []);
   return (
-    <div className="container check">
+    <div className="container check" id="check">
       <div className="line"></div>
       <h2 style={{ textAlign: "center" }}>Чайхана “Аламедин”</h2>
       <div className="line"></div>
@@ -21,11 +36,11 @@ const Check = ({ data, setShowCheck }) => {
         <li>Кассир: Ислам Ташиев</li>
       </ul>
       <div className="line"></div>
-      <Table data={data} />
+      <Table data={newData} />
       <div className="line"></div>
       <div className="total">
         <p>Итого:</p>
-        <p>19000сом</p>
+        <p>{totalSum}сом</p>
       </div>
       <div style={{ textAlign: "center", marginTop: "80px" }}>
         <object type="image/svg+xml" data={qrCode}></object>
